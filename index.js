4652f67256d6c321c5653879ee95fd3a33dc932a
Kevin G <anmoron11@gmail.com>
2021-03-18 18:05:23 -0400

    added cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import cors from 'cors'
 
 const app = express();
 const port = process.env.PORT || 2323;
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({
